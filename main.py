c55f9903b1db6e1f47b3972142dbc2804b4d205e
ribonred <ribonred@gmail.com>
2023-07-20 18:06:16 +0700

    Changed Order and Exchange classes to use an enum for the side of the order, and updated the type hints for the Instrument and Order classes.
    # Aider chat conversation:

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+from enum import StrEnum
+
+
+class Side(StrEnum):
+    BUY = "buy"
+    SELL = "sell"
+
+
 class Instrument:
-    def __init__(self, id, name, price):
+    def __init__(self, id: str, name: str, price: float):
         self.id = id
         self.name = name
         self.price = price
 
+
 class Order:
-    def __init__(self, id, instrument_id, quantity, type):
+    def __init__(self, id: str, instrument: Instrument, quantity: float, side: Side):
         self.id = id
-        self.instrument_id = instrument_id
+        self.instrument_id = instrument
         self.quantity = quantity
-        self.type = type
-        self.status = 'open'
+        self.side = side
+        self.status = "open"
         self.total_price = None
+
 
 class OrderBook:
     def __init__(self):
@@ -25,6 +35,8 @@ class OrderBook:
 
     def get_total_value(self):
         return sum(order.total_price for order in self.orders)
+
+
 class Exchange:
     def __init__(self):
         self.order_book = []
@@ -33,9 +45,19 @@ class Exchange:
         self.order_book.append(order)
 
     def match_orders(self):
-        for buy_order in [order for order in self.order_book if order.type == 'buy' and order.status == 'open']:
-            for sell_order in [order for order in self.order_book if order.type == 'sell' and order.status == 'open' and order.instrument_id == buy_order.instrument_id]:
+        for buy_order in [
+            order
+            for order in self.order_book
+            if order.type == "buy" and order.status == "open"
+        ]:
+            for sell_order in [
+                order
+                for order in self.order_book
+                if order.type == "sell"
+                and order.status == "open"
+                and order.instrument_id == buy_order.instrument_id
+            ]:
                 if sell_order.quantity == buy_order.quantity:
-                    buy_order.status = 'matched'
-                    sell_order.status = 'matched'
+                    buy_order.status = "matched"
+                    sell_order.status = "matched"
                     break

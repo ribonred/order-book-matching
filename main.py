b240d4eb353493be7721456a48425acd290f8df1
ribonred <ribonred@gmail.com>
2023-07-20 17:57:45 +0700

    aider: Modified the `Order` class to include a `type` attribute and a `status` attribute, and created a new `Exchange` class with an `order_book` attribute and a `match_orders` method.
    # Aider chat conversation:
    
    USER: how to run test?
    
    ASSISTANT: You can run the test using the Python unittest command-line interface. Open a terminal, navigate to the directory containing your test file, and run the following command:
    
    ```bash
    python -m unittest test_order.py
    ```
    
    This will run all the test methods in the `test_order.py` file. If all the tests pass, you will see an output similar to this:
    
    ```
    .....
    ----------------------------------------------------------------------
    Ran 5 tests in 0.001s
    
    OK
    ```
    
    If a test fails, the output will include details about the failure.
    
    USER: so this more like a cart. what i want is like exchange.. where there is sell and buy order.. inside that will be an `instrument` that can sell or buy.. and we can create Exchange entity that have job to pool or matching the order from order book
    
    ASSISTANT: I see, you want to create a simple exchange system where there are buy and sell orders for different instruments. This is a more complex system, but we can start by modifying our existing classes and adding new ones.
    
    First, we'll modify the `Order` class to include a `type` attribute that can be either 'buy' or 'sell'. We'll also add a `status` attribute that can be 'open', 'matched', or 'closed'.
    
    ```python
    main.py
    <<<<<<< ORIGINAL
    class Order:
        def __init__(self, id, instrument_id, quantity):
            self.id = id
            self.instrument_id = instrument_id
            self.quantity = quantity
            self.total_price = None
    =======
    class Order:
        def __init__(self, id, instrument_id, quantity, type):
            self.id = id
            self.instrument_id = instrument_id
            self.quantity = quantity
            self.type = type
            self.status = 'open'
            self.total_price = None
    >>>>>>> UPDATED
    ```
    
    Next, we'll create a new `Exchange` class. This class will have an `order_book` attribute that is a list of `Order` objects. It will also have a `match_orders` method that matches open buy and sell orders for the same instrument.
    
    ```python
    main.py
    <<<<<<< ORIGINAL
    
    =======
    class Exchange:
        def __init__(self):
            self.order_book = []
    
        def add_order(self, order):
            self.order_book.append(order)
    
        def match_orders(self):
            for buy_order in [order for order in self.order_book if order.type == 'buy' and order.status == 'open']:
                for sell_order in [order for order in self.order_book if order.type == 'sell' and order.status == 'open' and order.instrument_id == buy_order.instrument_id]:
                    if sell_order.quantity == buy_order.quantity:
                        buy_order.status = 'matched'
                        sell_order.status = 'matched'
                        break
    >>>>>>> UPDATED
    ```
    
    This is a very basic matching algorithm that only matches orders with the same quantity. In a real exchange, you would need a more complex algorithm that can handle partial matches and different order types.

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ class Instrument:
         self.price = price
 
 class Order:
-    def __init__(self, id, instrument_id, quantity):
+    def __init__(self, id, instrument_id, quantity, type):
         self.id = id
         self.instrument_id = instrument_id
         self.quantity = quantity
+        self.type = type
+        self.status = 'open'
         self.total_price = None
 
 class OrderBook:
@@ -23,3 +25,17 @@ class OrderBook:
 
     def get_total_value(self):
         return sum(order.total_price for order in self.orders)
+class Exchange:
+    def __init__(self):
+        self.order_book = []
+
+    def add_order(self, order):
+        self.order_book.append(order)
+
+    def match_orders(self):
+        for buy_order in [order for order in self.order_book if order.type == 'buy' and order.status == 'open']:
+            for sell_order in [order for order in self.order_book if order.type == 'sell' and order.status == 'open' and order.instrument_id == buy_order.instrument_id]:
+                if sell_order.quantity == buy_order.quantity:
+                    buy_order.status = 'matched'
+                    sell_order.status = 'matched'
+                    break

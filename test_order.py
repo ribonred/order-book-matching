a710c73d51ffabad87112faa12855ba153310064
ribonred <ribonred@gmail.com>
2023-07-20 18:07:03 +0700

    aider: Updated unit tests to reflect changes in the `Order` and `Exchange` classes, including adding a new test for matching orders.
    # Aider chat conversation:
    
    USER: im updating the code.. can you match it and renew the unit test
    
    ASSISTANT: Sure, I see that you've made some changes to the `Order` and `Exchange` classes. Let's update the unit tests to reflect these changes.
    
    First, we'll update the `setUp` method in the `TestOrderBook` class to use the new `Side` enum and the `instrument` attribute in the `Order` class. We'll also add a `test_match_orders` method to test the `match_orders` method in the `Exchange` class.
    
    ```python
    test_order.py
    <<<<<<< ORIGINAL
    class TestOrderBook(unittest.TestCase):
        def setUp(self):
            self.instrument = Instrument(1, 'Instrument1', 100)
            self.order = Order(1, self.instrument.id, 2)
            self.order.total_price = self.instrument.price * self.order.quantity
            self.order_book = OrderBook()
    
        def test_add_order(self):
            self.order_book.add_order(self.order)
            self.assertIn(self.order, self.order_book.orders)
    
        def test_remove_order(self):
            self.order_book.add_order(self.order)
            self.order_book.remove_order(self.order.id)
            self.assertNotIn(self.order, self.order_book.orders)
    
        def test_get_total_value(self):
            self.order_book.add_order(self.order)
            expected_total_value = self.order.total_price
            self.assertEqual(self.order_book.get_total_value(), expected_total_value)
    =======
    class TestOrderBook(unittest.TestCase):
        def setUp(self):
            self.instrument = Instrument('1', 'Instrument1', 100)
            self.buy_order = Order('1', self.instrument, 2, Side.BUY)
            self.sell_order = Order('2', self.instrument, 2, Side.SELL)
            self.buy_order.total_price = self.instrument.price * self.buy_order.quantity
            self.sell_order.total_price = self.instrument.price * self.sell_order.quantity
            self.order_book = OrderBook()
            self.exchange = Exchange()
    
        def test_add_order(self):
            self.exchange.add_order(self.buy_order)
            self.assertIn(self.buy_order, self.exchange.order_book)
    
        def test_remove_order(self):
            self.exchange.add_order(self.buy_order)
            self.exchange.remove_order(self.buy_order.id)
            self.assertNotIn(self.buy_order, self.exchange.order_book)
    
        def test_get_total_value(self):
            self.exchange.add_order(self.buy_order)
            expected_total_value = self.buy_order.total_price
            self.assertEqual(self.exchange.get_total_value(), expected_total_value)
    
        def test_match_orders(self):
            self.exchange.add_order(self.buy_order)
            self.exchange.add_order(self.sell_order)
            self.exchange.match_orders()
            self.assertEqual(self.buy_order.status, 'matched')
            self.assertEqual(self.sell_order.status, 'matched')
    >>>>>>> UPDATED
    ```
    
    This test now creates a buy order and a sell order in the `setUp` method, and tests that they are correctly matched by the `match_orders` method.

--- test_order.py
+++ test_order.py
@@ -3,24 +3,34 @@ from main import Instrument, Order, OrderBook
 
 class TestOrderBook(unittest.TestCase):
     def setUp(self):
-        self.instrument = Instrument(1, 'Instrument1', 100)
-        self.order = Order(1, self.instrument.id, 2)
-        self.order.total_price = self.instrument.price * self.order.quantity
+        self.instrument = Instrument('1', 'Instrument1', 100)
+        self.buy_order = Order('1', self.instrument, 2, Side.BUY)
+        self.sell_order = Order('2', self.instrument, 2, Side.SELL)
+        self.buy_order.total_price = self.instrument.price * self.buy_order.quantity
+        self.sell_order.total_price = self.instrument.price * self.sell_order.quantity
         self.order_book = OrderBook()
+        self.exchange = Exchange()
 
     def test_add_order(self):
-        self.order_book.add_order(self.order)
-        self.assertIn(self.order, self.order_book.orders)
+        self.exchange.add_order(self.buy_order)
+        self.assertIn(self.buy_order, self.exchange.order_book)
 
     def test_remove_order(self):
-        self.order_book.add_order(self.order)
-        self.order_book.remove_order(self.order.id)
-        self.assertNotIn(self.order, self.order_book.orders)
+        self.exchange.add_order(self.buy_order)
+        self.exchange.remove_order(self.buy_order.id)
+        self.assertNotIn(self.buy_order, self.exchange.order_book)
 
     def test_get_total_value(self):
-        self.order_book.add_order(self.order)
-        expected_total_value = self.order.total_price
-        self.assertEqual(self.order_book.get_total_value(), expected_total_value)
+        self.exchange.add_order(self.buy_order)
+        expected_total_value = self.buy_order.total_price
+        self.assertEqual(self.exchange.get_total_value(), expected_total_value)
+
+    def test_match_orders(self):
+        self.exchange.add_order(self.buy_order)
+        self.exchange.add_order(self.sell_order)
+        self.exchange.match_orders()
+        self.assertEqual(self.buy_order.status, 'matched')
+        self.assertEqual(self.sell_order.status, 'matched')
 
 if __name__ == '__main__':
     unittest.main()

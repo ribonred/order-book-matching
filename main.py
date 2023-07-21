02bfa3343c27940654dd95c2c05b49c3f189a2c2
ribonred <ribonred@gmail.com>
2023-07-21 07:29:53 +0700

    Refactored the `Order` class to include a `price` attribute and a `__str__` method, and added methods to the `Exchange` class to get buy and sell orders and to print the order book.
    # Aider chat conversation:

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 from enum import Enum
+from tabulate import tabulate
+import locale
 
-
+locale.setlocale(locale.LC_ALL, 'en_US')
 class Side(Enum):
     BUY = "BUY"
     SELL = "SELL"
 
 
 class Instrument:
-    def __init__(self, id: str, name: str, price: float):
+    def __init__(self, id: str, name: str):
         self.id = id
         self.name = name
-        self.price = price
 
 
 class Order:
-    def __init__(self, id: str, instrument: Instrument, quantity: float, side: Side):
+    def __init__(
+        self, id: str, instrument: Instrument, quantity: float, side: Side, price: float
+    ):
         self.id = id
-        self.instrument_id = instrument
+        self.instrument = instrument
         self.quantity = quantity
         self.side = side.value
         self.status = "open"
-        self.total_price = None
+        self.price = price
+    
+    def __str__(self) -> str:
+        return f"{self.quantity} @{locale.currency(self.price, grouping=True)}"
 
 
 class OrderBook:
     def __init__(self):
-        self.orders : list[Order] = []
+        self.orders: list[Order] = []
 
     def add_order(self, order):
         self.orders.append(order)
@@ -34,21 +40,7 @@ class OrderBook:
         self.orders = [order for order in self.orders if order.id != order_id]
 
     def get_total_value(self):
-        return sum(order.total_price for order in self.orders)
-
-
-class OrderBook:
-    def __init__(self):
-        self.orders : list[Order] = []
-
-    def add_order(self, order):
-        self.orders.append(order)
-
-    def remove_order(self, order_id):
-        self.orders = [order for order in self.orders if order.id != order_id]
-
-    def get_total_value(self):
-        return sum(order.total_price for order in self.orders)
+        return sum(order.price for order in self.orders)
 
     def match_orders(self):
         for buy_order in [
@@ -61,16 +53,42 @@ class OrderBook:
                 for order in self.orders
                 if order.side == Side.SELL.value
                 and order.status == "open"
-                and order.instrument_id == buy_order.instrument_id
+                and order.instrument == buy_order.instrument
             ]:
                 if sell_order.quantity == buy_order.quantity:
                     buy_order.status = "matched"
                     sell_order.status = "matched"
                     break
 
+
 class Exchange:
     def __init__(self):
         self.order_book: OrderBook = OrderBook()
+
+    def get_order(self, side: Side):
+        return [
+            order
+            for order in self.order_book.orders
+            if order.side == side and order.status == "open"
+        ]
+
+    def get_sell_order(self):
+        return self.get_order(Side.SELL.value)
+    
+    def get_buy_order(self):
+        return self.get_order(Side.BUY.value)
+    
+    def screen(self):
+        buys = self.get_buy_order()
+        sells = self.get_sell_order()
+        table_book = tabulate(
+            {
+                "buy": buys,
+                "sell": sells
+            },
+            headers="keys"
+        )
+        print(table_book)
 
     def add_order(self, order: Order):
         self.order_book.add_order(order)
